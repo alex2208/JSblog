a3b3bae870da8abc9a93bbec77927c81b2ac9bdd
alex2208 <37144355+alex2208@users.noreply.github.com>
2018-03-29 10:08:48 +0300

    blog added

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define('User', {
+        email: {
+            type: Sequelize.STRING,
+            required: true,
+            unique: true,
+            allowNull: false
+        },
+        passwordHash: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        fullName: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        salt: {
+            type: Sequelize.STRING,
+            required: true
+        },
+    }, {
+        timestamps: false
+    });
+    User.associate = (models) => {
+        User.hasMany(models.Article, {
+            foreignKey: 'authorId',
+            sourceKey: 'key'
+        });
+};
+    return User;
+};
